61632df4d84dea9a5b7e29f5f8023a80a3e1368d
dinan26 <dindadinanti26@gmail.com>
2024-09-17 21:11:40 +0700

    Tugas Individu 3

--- main/models.py
+++ main/models.py
@@ -1,9 +1,10 @@
+import uuid
 from django.db import models
 
 class Product(models.Model):
-    name = models.CharField(max_length = 255)
-    price = models.IntegerField()
-    description = models.TextField()
-    stock = models.IntegerField()
-    rating = models.DecimalField(max_digits= 3, decimal_places=2, null= True, blank= True)
-
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    nama = models.CharField(max_length = 255)
+    deskripsi = models.TextField()
+    stock =  models.IntegerField()
+    harga = models.IntegerField()
+   
